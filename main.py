6bdcf837dad435eb30e4c6788b37af7863b3d93c
Pero79 <32143031+Pero79@users.noreply.github.com>
2017-11-08 20:46:24 +0100

    some changes... still no season count
    Added chenges to write file so u can see all numbers not only sum

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ soup = BeautifulSoup(response)
 
 print soup.title.string + "\n"
 file = open("GoTviewers.txt", "w+")
-file.write("All wievs of GoT: ")
+file.write("All episode wievs of GoT: " + "\n")
 
 wikitable = soup.findAll("table", attrs={"class": "wikitable"})
 
@@ -18,10 +18,14 @@ for tr in wikitable[1].findAll("tr"):
     for td in tr.findAll("td"):
         table_data = td.string
         if table_data is not None and table_data != "N/A":
+            one_season_wievs = str(table_data)
             seasons_views_sum += float(table_data)
+            file.write(one_season_wievs + "\n")
+
 
 total_sum_views = str(seasons_views_sum)
 print "All wiews GoT: " + total_sum_views
+file.write("All wievs of GoT: ")
 file.write(total_sum_views)
 
 file.close()
